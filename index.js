a2703daa0dc7006886d8a3495e4084afc191249b
Echo-BotDev <yellowycorn6963@gmail.com>
2020-12-28 21:53:31 -0600

    edit

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const Bot = require("./base/Bot"),
     mongoose = require("mongoose");
 
 const client = new Bot({ disableMentions: "everyone" });
+const website = require("../website/app");
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 client.login(client.config.token);
-
+website.init();
 
 client.on("ready", () => {
     console.log(`${client.user.username} ready`);
