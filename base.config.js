d0d9e78a114094e167695521c880cf76ad36f772
Echo-BotDev <64450187+Echo-BotDev@users.noreply.github.com>
2020-12-30 16:54:18 -0800

    Update base.config.js

--- base.config.js
+++ base.config.js
@@ -2,15 +2,6 @@ module.exports = {
     prefix: "",
     token: "",
     users: "",
-    mongoDBuri: "",
-
-    Dashboard: {
-        enabled: false,
-        secret: "",
-        baseURL: "",
-        logs: "",
-        port: 8080,
-        expressSessionPassword: "H3y^dbg&043E",
-        failureURL: ""
+    mongoDBuri: ""
     }
 }
